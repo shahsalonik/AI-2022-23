a5a2537c64efa8c487be090b6c98ce9aedc6e41a
Saloni Shah <2023sshah@tjhsst.edu>
2022-08-24 16:14:34 -0400

    finished python_basics2

--- 2 Shah Saloni python_basics2.py
+++ 2 Shah Saloni python_basics2.py
@@ -3,35 +3,51 @@ import sys
 s = sys.argv[1]
 
 print("#1", s[2])
+
 print("#2", s[6])
+
 print("#3", len(s))
+
 print("#4", s[0])
+
 print("#5", s[len(s) - 1])
+
 print("#6", s[len(s) - 2])
+
 print("#7", s[3:8])
+
 print("#8", s[-5:])
+
 print("#9", s[2:])
+
 print("#10", s[::2])
+
 print("#11", s[1::3])
+
 print("#12", s[::-1])
+
 print("#13", s.find(" "))
+
 print("#14", s[0:-1])
+
 print("#15", s[1:])
+
 print("#16", s.lower())
 
-#TODO: figure out how to remove the empty string from the list, 17, 18
-print("#17", s.split(" "))
-print("#18", len(s.split(" ")))
+print("#17", s.split())
+
+print("#18", len(s.split()))
 
 print("#19", list(s))
 
-#TODO: ascii order, 20
-#print("#20", ord(s))
+new_str = ""
+for index, value in enumerate(sorted(s)):
+    new_str += str(value)    
+print("#20", new_str)
 
 print("#21", s[0:s.find(" ")])
 
-#TODO: palindrome, 22
-if(s == reverse(s)):
+if(s == s[::-1]):
     print("#22", True)
 else:
     print("#22", False)
