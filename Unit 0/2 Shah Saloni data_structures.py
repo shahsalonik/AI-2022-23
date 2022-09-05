408b50dd19c365e57f7b96974ba0fbb22512bd5c
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2022-09-05 16:17:54 -0400

    #6 Works!

--- Unit 0/2 Shah Saloni data_structures.py
+++ Unit 0/2 Shah Saloni data_structures.py
@@ -3,11 +3,13 @@ from multiprocessing import heap
 from time import perf_counter
 from heapq import heappush, heappop, heapify
 
+import sys
+
 start = perf_counter()
 
 #code here
 
-f1, f2, f3 = "10mfile1.txt", "10mfile2.txt", "10mfile3.txt"
+f1, f2, f3 = sys.argv[1], sys.argv[2], sys.argv[3]
 s1, s2, s3 = [], [], []
 set3 = set()
 
@@ -51,7 +53,6 @@ for i in set1:
 print("#1: %s" % count)
 
 #Problem 2: Sum of every 100th unique value file 1
-
 index = 99 
 sum = 0
 list1 = list(dict1.keys())
@@ -63,7 +64,6 @@ while index < len(list1):
 print("#2: %s" % sum)
 
 #Problem 3: Total number of times each unique value in file 3 appears in files 1/2
-
 count_3 = 0
 
 for key in set3:
@@ -75,7 +75,6 @@ for key in set3:
 print("#3: %s" % count_3)
 
 #Problem 4: 10 smallest numbers in file 1 in increasing order
-
 sorted_queue = deque(set1)
 small_list = []
 
@@ -84,7 +83,6 @@ for y in range(10):
 print("#4: %s" % small_list)
 
 #Problem 5: 10 largest numbers appearing 2+ times in file 2 in decreasing order
-
 new_s2 = []
 big_list = []
 
@@ -97,6 +95,32 @@ sorted_s2 = sorted(new_s2)
 for k in range(10):
     big_list.append(sorted_s2.pop())
 print("#5: %s" % big_list)
+
+#Problem 6: Read file 1 and get the smallest numbers before/including a multiple of 53 in a heap, then add them
+sum_53 = 0
+new_s1 = set()
+heap_list = []
+
+for x in s1:
+    if x % 53 != 0:
+        if x not in new_s1:
+            heappush(heap_list, x)
+    else:
+        if x not in new_s1:
+            heappush(heap_list, x)
+        temp = heappop(heap_list)
+        if temp in new_s1:
+            while temp in new_s1:
+                temp = temp = heappop(heap_list)
+            new_s1.add(temp)
+        else:
+            new_s1.add(temp)
+        
+for y in new_s1:
+    sum_53 += y
+
+#31073
+print("#6: %s" % sum_53)
 
 end = perf_counter()
 print("Total time:", end - start)
