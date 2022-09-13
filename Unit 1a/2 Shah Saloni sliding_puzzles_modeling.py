76f8fae585e4338c7e4d14ad5311a4fddd4a5481
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2022-09-13 11:26:22 -0400

    print_puzzle and find_goal

--- Unit 1a/2 Shah Saloni sliding_puzzles_modeling.py
+++ Unit 1a/2 Shah Saloni sliding_puzzles_modeling.py
@@ -1,5 +1,16 @@
-with open("slide_puzzles_tests.txt") as f:
+with open("slide_puzzle_tests.txt") as f:
     line_list = [line.strip() for line in f]
 
-def print_puzzle(size, puzzle_rep):
-    print(size, puzzle_rep)
+def print_puzzle(size, board):
+    for num in range(size):
+        print((" ").join(board[:size]))
+        board = board[size:]
+
+def find_goal(board):
+    sorted_board = sorted(board)
+    goal_state = sorted_board[1:]
+    goal_state.append(".")
+    return goal_state
+
+print_puzzle(2, "A.CB")
+find_goal("A.CB")
