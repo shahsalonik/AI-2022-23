b2a73fb4c293b861987711de344e440fd2aefde6
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2022-09-15 10:25:31 -0400

    Update 2 Shah Saloni sliding_puzzles_modeling.py

--- Unit 1a/2 Shah Saloni sliding_puzzles_modeling.py
+++ Unit 1a/2 Shah Saloni sliding_puzzles_modeling.py
@@ -1,10 +1,14 @@
 with open("slide_puzzle_tests.txt") as f:
     line_list = [line.strip() for line in f]
 
+with open("slide_puzzle_tests.txt") as f:
+    line_list = [line.strip() for line in f]
+
 def print_puzzle(size, board):
     for num in range(size):
         print((" ").join(board[:size]))
         board = board[size:]
+    print(board)
 
 def find_goal(board):
     sorted_board = sorted(board)
@@ -14,9 +18,17 @@ def find_goal(board):
 
 def get_children(state):
     blank_index = state.index(".")
-    print(blank_index)
+    #print(blank_index)
 
 #left right +1, -1
 #up,down +size, -size
 
-get_children("A.CB")
+count = 0
+
+for x in line_list:
+    size = int(x[0])
+    print("Line", count, "start state:")
+    print_puzzle(size, x[2:])
+    print("Line", count, "goal state:", find_goal(x[2:]))
+    print("Line", count, "children:", get_children(x[2:]), "\n")
+    count += 1
