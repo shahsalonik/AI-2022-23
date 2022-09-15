ee96966a61f0a8bc0872e1d5d91f30744acf9647
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2022-09-15 11:13:33 -0400

    sliding puzzles works!

--- Unit 1a/2 Shah Saloni sliding_puzzles_modeling.py
+++ Unit 1a/2 Shah Saloni sliding_puzzles_modeling.py
@@ -1,9 +1,6 @@
 with open("slide_puzzle_tests.txt") as f:
     line_list = [line.strip() for line in f]
 
-with open("slide_puzzle_tests.txt") as f:
-    line_list = [line.strip() for line in f]
-
 def print_puzzle(size, board):
     for num in range(size):
         print((" ").join(board[:size]))
@@ -17,18 +14,26 @@ def find_goal(board):
     return ''.join(goal_state)
 
 def get_children(state):
-    blank_index = state.index(".")
-    #print(blank_index)
-
-#left right +1, -1
-#up,down +size, -size
+    size = int(state[0])
+    board = state[2:]
+    blank_index = board.index(".")
+    children = []
+    if blank_index % size != 0: #blank swap right
+        children.append(board[:blank_index - 1] + "." + board[blank_index - 1] + board[blank_index + 1:])
+    if (blank_index + 1) % size != 0: #blank swap left
+        children.append(board[:blank_index] + board[blank_index + 1] + "." + board[blank_index + 2:])
+    if blank_index >= size: #blank swap up
+        children.append(board[:blank_index - size] + "." + board[blank_index - size + 1:blank_index] + board[blank_index - size] + board[blank_index + 1:]) 
+    if blank_index <= (size * (size - 1)): #blank swap
+        children.append(board[:blank_index] + board[blank_index + size] + board[blank_index + 1:blank_index + size] + "." + board[blank_index + size + 1:])
+    return children
 
 count = 0
 
 for x in line_list:
     size = int(x[0])
     print("Line", count, "start state:")
-    print_puzzle(size, x[2:])
+    print_puzzle(size, x[2:]) 
     print("Line", count, "goal state:", find_goal(x[2:]))
-    print("Line", count, "children:", get_children(x[2:]), "\n")
+    print("Line", count, "children:", get_children(x), "\n")
     count += 1
