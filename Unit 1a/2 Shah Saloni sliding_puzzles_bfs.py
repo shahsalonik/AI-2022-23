ca1684324bbe137d606cb5d56d41c517a5917878
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2022-09-19 10:46:58 -0400

    Update 2 Shah Saloni sliding_puzzles_bfs.py

--- Unit 1a/2 Shah Saloni sliding_puzzles_bfs.py
+++ Unit 1a/2 Shah Saloni sliding_puzzles_bfs.py
@@ -1,24 +1,24 @@
-from collections import deque
 import queue
 import sys
 from time import perf_counter
 
 #sys.argv[1]
-filename = "AI-2022-23/Unit 1a/slide_puzzle_tests.txt"
+filename = "slide_puzzle_tests.txt"
 
 with open(filename) as f:
     line_list = [line.strip() for line in f]
 
 def find_goal(board):
+    size, board = board.split()
     sorted_board = sorted(board)
     goal_state = sorted_board[1:]
     goal_state.append(".")
     return ''.join(goal_state)
 
 def get_children(state):
-    size = int(state[0])
-    board = state[2:]
-    blank_index = board.index(".")
+    size, board = state.split()
+    size = int(size)
+    blank_index = int(board.index("."))
     children = []
     if blank_index % size != 0: #blank swap right
         children.append(board[:blank_index - 1] + "." + board[blank_index - 1] + board[blank_index + 1:])
@@ -26,38 +26,33 @@ def get_children(state):
         children.append(board[:blank_index] + board[blank_index + 1] + "." + board[blank_index + 2:])
     if blank_index >= size: #blank swap up
         children.append(board[:blank_index - size] + "." + board[blank_index - size + 1:blank_index] + board[blank_index - size] + board[blank_index + 1:]) 
-    if blank_index <= (size * (size - 1)): #blank swap down
+    if blank_index < (size * (size - 1)): #blank swap down
         children.append(board[:blank_index] + board[blank_index + size] + board[blank_index + 1:blank_index + size] + "." + board[blank_index + size + 1:])
     return children
 
 def BFS(start_node):
     fringe = deque()
     visited = set()
-    fringe.append(start_node)
+    fringe.append((0, start_node))
     visited.add(start_node)
-    moves = 1
-    while len(fringe) != 0:
+    while fringe:
         v = fringe.popleft()
-        if find_goal(v):
-            moves_list.append(moves)
-            moves = 0
+        print(v[0])
+        print(v[1])
+        if v[1] == find_goal(v[1]):
             return v
-        else:
-            for x in get_children(v):
-                if x not in visited:
-                    fringe.append(x)
-                    visited.add(x)
-                    moves += 1
+        for x in get_children(v[1]):
+            if x not in visited:
+                fringe.append((v[0] + 1, x))
+                visited.add(x)
     return None
 
 count = 0
-moves_list = []
 
 for x in line_list:
-    size = int(x[0])
-    board = x[2:]
+    board = x
     start = perf_counter()
     solved_board = BFS(board)
     end = perf_counter()
-    print("Line", count, ":", BFS(board),  ",",  moves_list[count], "moves", "found in", end - start)
+    print("Line", count, ":", solved_board,  ",",  solved_board[0], "moves", "found in", end - start)
     count += 1
