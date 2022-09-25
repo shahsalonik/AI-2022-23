d5cbfa6738fc5bd3fc1c330c287307e3fc658904
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2022-09-25 18:46:22 -0400

    (very slow) working word ladders

--- Unit 1a/2 Shah Saloni word_ladders.py
+++ Unit 1a/2 Shah Saloni word_ladders.py
@@ -3,39 +3,92 @@ import queue
 import sys
 from time import perf_counter
 
+start = perf_counter()
+
 with open("words_06_letters.txt") as f:
     dict_list = [line.strip() for line in f]
 
 with open("puzzles_normal.txt") as f:
     puzzle_list = [line.strip() for line in f]
 
+#initializing the dict with each possible letter variation of each of its keys
+#need a dict where the values are lists and the keys are the root words
+#check if word differs by only 1 letter???
+#iterate through each index -> check if char is the same -> if yes do nothing
+#if no add smth? -> add to a running count of different letters and then just return that
+#should be called in the create word list func
+
+def create_dict(word_list):
+    word_dict = {}
+
+    for w in dict_list:
+        word_dict[w] = create_word_list(w, word_list)
+
+    return word_dict
+
+def create_word_list(word, dict):
+    word_list = []
+
+    for real_word in dict:
+        if check_word_diff(word, real_word) == 1:
+            word_list.append(real_word)
+    
+    return word_list
+
+def check_word_diff(word1, word2):
+    diff_letters = 0
+
+    for x in range(len(word1)):
+        if word1[x] != word2[x]:
+            diff_letters += 1
+    
+    return diff_letters
+
 #should take in each line and return both words (split)
 def goal_test(one_line):
     input, goal = one_line.split()
     return input, goal
 
-def get_children(node):
-    print("no children yet")
+def path(node, tracked_path):
+    path_list = [node]
 
-def wordLadders(first, goal):
+    while tracked_path[node] != "s":
+        path_list.append(tracked_path[node])
+        node = tracked_path[node]
+
+    return path_list[::-1]
+
+def word_ladders(first, goal, dict):
+    path_track_dict = {first: "s"}
     fringe = deque()
     visited = set()
-    fringe.append((0, first))
+
+    fringe.append(first)
     visited.add(first)
+
     while fringe:
         v = fringe.popleft()
-        if v[1] == goal:
-            return v
-        for x in get_children(v[1]):
-            if x not in visited:
-                fringe.append((v[0] + 1, x))
+        if v == goal:
+            return path(v, path_track_dict)
+        
+        children = dict[v]
+
+        for x in children:
+            if x not in path_track_dict.keys():
+                fringe.append(x)
                 visited.add(x)
-    return "Cannot be solved!"
+                path_track_dict[x] = v
+
+    return "No Solution!"
 
 count = 0
+dict = create_dict(dict_list)
 
 for x in puzzle_list:
     initial, final = goal_test(x)
-    ladder = wordLadders(initial, final)
-    print("Line", count, ":\nLength is:", len(ladder), "\n%s" %initial, "\n%s" %ladder, "\n%s" %final, "\n")
+    ladder = word_ladders(initial, final, dict)
+    print(ladder)
     count += 1
+
+end = perf_counter()
+print("Total time: ", end - start)
