d87d01273b7b6193dca097e9a55d1ea56f57a8a3
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2022-09-29 11:29:52 -0400

    bibfs questions

--- Unit 1a/2 Shah Saloni sliding_puzzles_bibfs.py
+++ Unit 1a/2 Shah Saloni sliding_puzzles_bibfs.py
@@ -11,25 +11,25 @@ with open(filename) as f:
     line_list = [line.strip() for line in f]
 
 def find_goal(board):
-    size, board = board.split()
+    
     sorted_board = sorted(board)
     goal_state = sorted_board[1:]
     goal_state.append(".")
     return ''.join(goal_state)
 
 def get_children(state):
-    size, board = state.split()
-    size = int(size)
+    board = state
+    size = int(4)
     blank_index = int(board.index("."))
     children = []
     if blank_index % size != 0: #blank swap right
-        children.append(str(size) + " " + board[:blank_index - 1] + "." + board[blank_index - 1] + board[blank_index + 1:])
+        children.append(board[:blank_index - 1] + "." + board[blank_index - 1] + board[blank_index + 1:])
     if (blank_index + 1) % size != 0: #blank swap left
-        children.append(str(size) + " " + board[:blank_index] + board[blank_index + 1] + "." + board[blank_index + 2:])
+        children.append(board[:blank_index] + board[blank_index + 1] + "." + board[blank_index + 2:])
     if blank_index >= size: #blank swap up
-        children.append(str(size) + " " + board[:blank_index - size] + "." + board[blank_index - size + 1:blank_index] + board[blank_index - size] + board[blank_index + 1:]) 
+        children.append(board[:blank_index - size] + "." + board[blank_index - size + 1:blank_index] + board[blank_index - size] + board[blank_index + 1:]) 
     if blank_index < (size * (size - 1)): #blank swap down
-        children.append(str(size) + " " + board[:blank_index] + board[blank_index + size] + board[blank_index + 1:blank_index + size] + "." + board[blank_index + size + 1:])
+        children.append(board[:blank_index] + board[blank_index + size] + board[blank_index + 1:blank_index + size] + "." + board[blank_index + size + 1:])
     return children
 
 def BFS(start_node):
@@ -76,6 +76,7 @@ def BFS(start_node):
                 solved_fringe_dict[y] = (s_v[0] + 1)
     return None
 
+
 count = 0
 
 for x in line_list:
@@ -88,3 +89,19 @@ for x in line_list:
 
 total_end = perf_counter()
 print("Total Time: %s" % (total_end - total_start))
+
+'''
+Part 2: 
+1. Bidirectional BFS runs at a fraction of the time as BFS (0.6s compared to 2.5s)
+2. The last puzzle I can solve in less than a minute is line 41, which almost double
+the number of puzzles I could solve with BFS, when it could only solve until line 22.
+3. BiBFS word ladders is only very slightly faster than normal word ladders
+4. I stored the word ladder with 2 different dicts, one that tracked the path from the beginning and one that tracked it from
+the end. Then, when the solution was found or either one of the words was in the other fringe, I returned a combination of both 
+paths using two different methods: ```end_path``` and ```path```. ```path``` returned a reversed list of the dict's values, 
+and ```end_path``` returned a non-reversed list of the dict's values. This is because when I was tracking the paths, the 
+```end_path``` method already iterated through the dict and added the values to the dict in a reversed manner. 
+5. BiBFS is more useful when you have the final goal state and the children of the start node (and any following nodes) 
+grow at an exponential pace and are unknown. BiBFS was most useful for decreasing the time to solve sliding puzzles, because
+we didn't know the children, but in word ladders, the children were all already generated and stored in a dict.
+'''
