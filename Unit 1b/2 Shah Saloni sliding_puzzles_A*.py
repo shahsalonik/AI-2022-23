3ff451025119b0eae8c053688b6c53c8d9166b56
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2022-10-04 11:40:17 -0400

    working on parity check

--- Unit 1b/2 Shah Saloni sliding_puzzles_A*.py
+++ Unit 1b/2 Shah Saloni sliding_puzzles_A*.py
@@ -1,1 +1,37 @@
 import sys
+
+#sys.argv[1]
+filename = "slide_puzzle_tests.txt"
+
+with open(filename) as f:
+    line_list = [line.strip() for line in f]
+
+#parity check
+def is_solvable(state):
+    size, board = state.split()
+    blank_index = int(board.index("."))
+    board = board[:blank_index] + board[blank_index + 1:]
+    size = int(size)
+
+    out_of_order = 0
+
+    if size % 2 != 0:
+        for x in range(len(board) - 1):
+            for y in board[x:]:
+                if ord(board[x]) > ord(y):
+                    out_of_order += 1
+        
+        if out_of_order % 2 == 0:
+            return True
+    else:
+        for x in range(len(board) - 1):
+            for y in board[x:]:
+                if ord(board[x]) > ord(y):
+                    out_of_order += 1
+
+        #find rows by splitting the board into its sizes
+        return True
+    return False
+
+for puzzle in line_list:
+    print(puzzle, is_solvable(puzzle))
