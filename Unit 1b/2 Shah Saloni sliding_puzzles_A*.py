8d672354a2b84b0d562dda584aba46ebdbb5a789
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2022-10-04 18:20:00 -0400

    finished parity check, taxicab

--- Unit 1b/2 Shah Saloni sliding_puzzles_A*.py
+++ Unit 1b/2 Shah Saloni sliding_puzzles_A*.py
@@ -1,10 +1,16 @@
 import sys
 
 #sys.argv[1]
-filename = "slide_puzzle_tests.txt"
+filename = "15_puzzles.txt"
 
 with open(filename) as f:
     line_list = [line.strip() for line in f]
+
+def find_goal(board):
+    sorted_board = sorted(board)
+    goal_state = sorted_board[1:]
+    goal_state.append(".")
+    return ''.join(goal_state)
 
 #parity check
 def is_solvable(state):
@@ -28,10 +34,37 @@ def is_solvable(state):
             for y in board[x:]:
                 if ord(board[x]) > ord(y):
                     out_of_order += 1
-
+        
+        row_count = 0
         #find rows by splitting the board into its sizes
-        return True
+        for row in range(size):
+            size_set = set()
+            size_set.add(board[row:row + size + 1])
+            if "." in size_set:
+                break
+            else:
+                row_count += 1
+        
+        if row_count % 2 == 0:
+            if out_of_order % 2 != 0:
+                return True
+        else:
+            if out_of_order % 2 == 0:
+                return True
     return False
 
+def taxicab(state):
+    moves = 0
+    goal = find_goal(state)
+    size = int(4)
+    for x in range(len(state)):
+        if not state[x] == ".":
+            x_point = goal.find(state[x]) // size
+            y_point = goal.find(state[x]) % size
+            x_distance = abs(x_point - x // size)
+            y_distance = abs(y_point - x % size)
+            moves += x_distance + y_distance
+    return moves
+
 for puzzle in line_list:
-    print(puzzle, is_solvable(puzzle))
+    print(puzzle, taxicab(puzzle))
