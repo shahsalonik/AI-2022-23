c07b10a4148e7536cb1167e7db1b5bb1529f5e5b
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2022-10-20 11:57:10 -0400

    Update 2 Shah Saloni n_queens.py

--- Unit 2/2 Shah Saloni n_queens.py
+++ Unit 2/2 Shah Saloni n_queens.py
@@ -1,8 +1,14 @@
-
 import sys
 from time import perf_counter
 
 start = perf_counter()
+
+def generate_board(size):
+    board = []
+    for x in range(size):
+        board.append(None)
+
+    return board
 
 def goal_test(state):
     if None not in state:
@@ -14,18 +20,38 @@ def get_next_unassigned_var(state):
     return none_index
 
 def get_sorted_values(state, var):
-    return ""
+    children = []
+    size = len(state)
+    is_valid = True
+    for n in range(size):
+        count = 0
+        if n in state:
+            is_valid = False
+        while is_valid == True and var == n - count:
+            if ((n - count) >= 0) :
+                is_valid = False
+            count += 1
+    if is_valid:
+        children.append(n)
+    return children
+            
 
 def csp_backtracking(state):
     if goal_test(state):
         return state
     var = get_next_unassigned_var(state)
     for val in get_sorted_values(state, var):
-        val = var
-        result = csp_backtracking(val)
+        new_state = state.copy()
+        new_state[var] = val
+        result = csp_backtracking(new_state)
         if result is not None:
             return result
     return None
 
 end = perf_counter()
+
+print(generate_board(4))
+board = generate_board(4)
+print(csp_backtracking(board))
+
 print("Total time: %s" % (end - start))
