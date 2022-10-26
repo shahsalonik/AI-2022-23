4c1b868279a97330b59ee3e2931b168a4827105e
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2022-10-26 19:11:55 -0400

    wokring on inc repair

--- Unit 2/2 Shah Saloni n_queens_inc_repair.py
+++ Unit 2/2 Shah Saloni n_queens_inc_repair.py
@@ -22,14 +22,77 @@ def test_solution(state):
                 return False
     return True
 
-def generate_board(size):
+def inc_repair_generate_board(size):
     board = [i for i in range(size)]
     for i in range(len(board)):
         board[i] = random.choice(board)
     return board
 
-def goal_test(conflicts_sum):
+def inc_repair_goal_test(conflicts_sum):
     if conflicts_sum == 0:
+        return True
+    return False
+
+def conflicts(state, index):
+    return ""
+
+#pick from middle bc it's more efficient
+#use a min heap 
+def inc_repair_get_next_unassigned_var(state):
+    size = len(state)
+    heap_values =[(abs(size // 2 - index), index) for index in range(size) if state[index] == None]
+    heapify(heap_values)
+    return heappop(heap_values)[1]
+
+def inc_repair_get_sorted_values(state, var):
+    index = state[var]
+    children = []
+    heapify(children)
+    size = len(state)
+
+    for i in range(size):
+        if index != i:
+            new_state = state.copy()
+            new_state[var] = i
+            
+            conflict = conflicts(new_state, var)
+            heappush((conflict, i))
+
+    return children
+
+def inc_repair_backtracking(state, conflicts):
+    size = len(state)
+    total_conflicts = sum(conflicts)
+
+    if inc_repair_goal_test(total_conflicts):
+        return state
+
+    print("Current board state: ", state)
+    print("Conflicts: ", total_conflicts)
+
+    var = inc_repair_get_next_unassigned_var(state, conflicts)
+
+    for conf, val in inc_repair_get_sorted_values(state, conflicts):
+        new_state = state.copy()
+        new_state[var] = val
+
+        result = inc_repair_backtracking(new_state, [**SOME CONDITION** for i in range(len(state))])
+
+        if result is not None:
+            return result
+    return None
+
+#########NORMAL BACKTRACKING############
+
+def generate_board(size):
+    board = [None for x in range(size)]
+    column = [None for x in range(size)]
+    diagonal = [None for x in range(size * 2 - 1)]
+    diagonal2 = diagonal.copy()
+    return board, column, diagonal, diagonal2
+
+def goal_test(state):
+    if None not in state:
         return True
     return False
 
@@ -60,23 +123,23 @@ def get_sorted_values(state, var, column, left_diag, right_diag):
     #reversed list works better
     return children[::-1]
 
-def csp_backtracking(state, conflicts):
+def csp_backtracking(state, column, diagonal, diagonal2):
     size = len(state)
-    total_conflicts = sum(conflicts)
-
-    if goal_test(total_conflicts):
+    if goal_test(state):
         return state
-
-    print("Current board state: ", state)
-    print("Conflicts: ", total_conflicts)
-
-    var = get_next_unassigned_var(state, conflicts)
-
-    for conf, val in get_sorted_values(state, conflicts):
+    var = get_next_unassigned_var(state)
+    for val in get_sorted_values(state, var, column, diagonal, diagonal2):
         new_state = state.copy()
-        new_state[var] = val
+        new_column = column.copy()
+        new_diagonal = diagonal.copy()
+        new_diagonal2 = diagonal2.copy()
 
-        result = csp_backtracking(new_state, [**SOME CONDITION** for i in range(len(state))])
+        new_state[var] = val
+        new_column[val] = val
+        new_diagonal[var + val] = val
+        new_diagonal2[var - 1 - val + size] = val
+
+        result = csp_backtracking(new_state, new_column, new_diagonal, new_diagonal2)
 
         if result is not None:
             return result
