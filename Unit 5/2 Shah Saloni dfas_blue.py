3e32ed5fe4edde8266837f6ff6328f97d07d8a5d
Saloni Shah <59715228+shahsalonik@users.noreply.github.com>
2023-02-28 22:08:23 -0500

    finished part 1

--- Unit 5/2 Shah Saloni dfas_blue.py
+++ Unit 5/2 Shah Saloni dfas_blue.py
@@ -1,6 +1,8 @@
 import sys
 
 filename = sys.argv[1]
+tests = sys.argv[2]
+
 language = ''
 states = 0
 final_states = []
@@ -13,6 +15,8 @@ with open(filename) as f:
     language = str(inputs[0])
     states = inputs[1]
     final_states = inputs[2].split()
+    for z in range(len(final_states)):
+        final_states[z] = int(final_states[z])
 
     for x in range(1, len(file_string_list)):
         temp_list = file_string_list[x].split("\n")
@@ -25,4 +29,38 @@ with open(filename) as f:
             state_dict[letter_list[0]] = int(letter_list[1])
         dfa_dict[int(temp_list[0])] = state_dict
 
-print(dfa_dict)
+def print_table(nested_dict):
+    language_list = [x for x in language]
+    print("*\t\t", "\t\t".join(language_list))
+    for key in nested_dict.keys():
+        print_list = [str(key)]
+        for y in language_list:
+            if y in nested_dict[key].keys():
+                print_list.append(str(nested_dict[key][y]))
+            else:
+                print_list.append("_")
+        print("\t\t".join(print_list))
+
+print_table(dfa_dict)
+print("Final nodes:", final_states)
+
+def dfa_check_helper(current, character):
+    if character in dfa_dict[current]:
+        new_state = dfa_dict[current][character]
+    else:
+        new_state = None
+    return new_state
+
+def dfa_check(input_string, state):
+    current_state = state
+    for char in input_string:
+        current_state = dfa_check_helper(current_state, char)
+        if current_state == None:
+            return False
+    if current_state in final_states:
+        return True
+
+with open(tests) as t:
+    for line in t:
+        line = line.strip("\n")
+        print(dfa_check(line, 0), line)
